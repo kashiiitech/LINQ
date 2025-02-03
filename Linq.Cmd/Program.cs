30414ef03ca197a7075fd78bcc01d0160ce66f50
kashiiitech <kashiiitech@gmail.com>
2025-02-03 16:01:19 +0500

    deffered execution in LINQ: different methods(ways)

--- Linq.Cmd/Program.cs
+++ Linq.Cmd/Program.cs
@@ -1,3 +1,5 @@
-﻿using Linq.Cmd._3_FilteringAndOrdering;
+﻿using Linq.Cmd._4_OutputFromQueries;
+using Linq.Cmd._3_FilteringAndOrdering;
+using Linq.Cmd._4_OutputFromQueries;
 
-new Ordering().Run();
+new DeferredExecution().Run();
